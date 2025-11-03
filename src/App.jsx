d606237073990c1c8433d236606eb01290b8b549
GeorgeKandelaki <kandelakig29@hotmail.com>
2025-11-03 10:42:44 +0400

    adding

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+    return (
+        <>
+            <GlobalStyles />
+            <div></div>;
+        </>
+    );
+}
+
+export default App;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    :root {}
+
+    html{
+        font-size: 62.5%;
+    }
+
+    body{}
+`;
+
+export default GlobalStyles;
